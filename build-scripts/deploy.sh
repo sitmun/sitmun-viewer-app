8a7be3c5b08c77a6dc0963062a31dab42ceb9133
Rubén Béjar <rbejar@unizar.es>
2023-07-14 12:10:49 +0200

    Automatic deployment.

---
+++ build-scripts/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo
+echo "Deploying to testdeployment configuration ..."
+echo
+
+if [ -n "$GITHUB_API_KEY" ]; then
+     mkdir tmp
+     cd tmp
+     git clone https://github.com/sitmun/sitmun.github.io.git
+     cd sitmun.github.io
+     cp -r "$GITHUB_WORKSPACE"/dist/viewer-app ./docs
+     # In GitHub Actions, set user and email for git repo
+     if [ -n "$CI" ]; then
+        git config user.name "GitHub Actions Bot"
+        git config user.email "<>"
+     fi
+     git add admin-app/*
+     git commit -m "Deployment of the sitmun-viewer-app client"
+#    # Make sure to make the output quiet, or else the API token will leak!
+#    # This works because the API key can replace your password.
+      git push -q https://$USERNAME:$GITHUB_API_KEY@github.com/sitmun/sitmun.github.io master &>/dev/null
+     cd ../..
+     rm -r -f tmp
+fi
---
+++ src/environments/environment.testdeployment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://sitmun-backend-core.herokuapp.com'
+};
