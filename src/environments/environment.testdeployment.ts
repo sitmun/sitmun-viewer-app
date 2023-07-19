6d04ceea0fc418e2d517d322de8ef6b27f878b8d
Rubén Béjar <rbejar@unizar.es>
2023-07-19 12:49:06 +0200

    For now, keeping default LocationStrategy even on GitHub pages. The HashLocationStrategy prevents page refreshes from breaking the site, but it does seem to interfere with the map state management that SITNA API does."

--- src/environments/environment.testdeployment.ts
+++ src/environments/environment.testdeployment.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: false,
-  hashLocationStrategy: true,
+  hashLocationStrategy: false, // true would solve page refresh issues in GitHub pages,
+  // but currently it interferes with SITNA map state management:
+  // Map.js puts state changes in a hash fragment in the URL, but currently it
+  // erases any other hash in that URL (a bug?)
   apiUrl: 'https://sitmun-backend-core.herokuapp.com'
 };
