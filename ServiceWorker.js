b506a175cc1822f10b9b3f1279824e19314d8e1d
rbejar <>
2024-04-23 07:46:18 +0000

    Auto-generated commit [ci skip]
    Triggered by commit: https://github.com/sitmun/sitmun-viewer-app/commit/08428536bb62d78f9e3cddd14faad4e1b9975e41

---
+++ ServiceWorker.js
@@ -0,0 +1,62 @@
+
+const MIDDLEWARE_URL_KEY = "MIDDLEWARE_URL";
+const AUTH_TOKEN_KEY = "AUTH_TOKEN";
+
+var authToken = undefined;
+var middlewareUrl = undefined;
+
+self.addEventListener("install", (event) => {
+  console.log("Service worker installed");
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service worker activated");
+});
+
+self.addEventListener("message", (event) => {
+  const eventData = event.data;
+  if (eventData.type === AUTH_TOKEN_KEY) {
+    authToken = eventData.token;
+    console.log("Access token received");
+    return;
+  }
+
+  if (eventData.type === MIDDLEWARE_URL_KEY) {
+    middlewareUrl = eventData.url;
+    console.log("Middleware URL received:", middlewareUrl);
+    return;
+  }
+
+  console.log("Unknown message received:", eventData);
+});
+
+self.addEventListener("fetch", (event) => {
+  var request = event.request;
+  var url = new URL(request.url);
+
+  // Filter requests to only include middleware requests
+  if (!middlewareUrl || !url.toString().startsWith(middlewareUrl)) {
+    return;
+  }
+
+  // Check if we have an auth token to add to the request. If not, omit
+  if (authToken) {
+    const modifiedRequest = new Request(request, {
+      headers: new Headers({
+        ...request.headers,
+        "Authorization": `Bearer ${authToken}`
+      }),
+      credentials: "omit",
+      mode: "cors"
+    });
+
+    console.log("Sending modified request to", modifiedRequest.url);
+    event.respondWith(fetch(modifiedRequest))
+      .then(response => { return response; });
+  } else {
+    console.log("No auth token found, sending original request");
+    event.respondWith(fetch(request))
+      .then(response => { return response; })
+  }
+});
