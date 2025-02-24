93cdb2cd7ca2b6fdaeb34a99fbb0a7b7037aa277
damien chabret <dch@atolcd.com>
2025-02-24 13:20:37 +0100

    added reusable components + updated SCSS files
    -> primary-button : primary button
    -> secondary-button : secondary button
    -> form-field-input : form field
    -> modification and addition of global SCSS variables
    
    Change-Id: I1a182f46d59b7a68a7ee095854fbb61ee05ae5e1

---
+++ src/app/ui/components/form-field-input/form-field-input.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'form-field-input',
+  templateUrl: './form-field-input.component.html',
+  styleUrls: ['./form-field-input.component.scss']
+})
+export class FormFieldInputComponent {
+  @Input() input_placeholder: string = '';
+  @Input() input_model: string = '';
+  @Input() input_name: string = '';
+  @Input() input_type: string = '';
+  @Input() input_title: string = '';
+
+  @Output() output_model = new EventEmitter<string>();
+
+  ngOnChanges(): void {
+    this.output_model.emit(this.input_model);
+  }
+
+  onInputChange(): void {
+    this.output_model.emit(this.input_model);
+  }
+}
--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -10,6 +10,9 @@ import { DialogFilterPipe } from '@sections/common/modals/dashboard-modal/dialog
 import { MenuComponent } from './components/menu/menu.component';
 import { DashboardPaginationComponent } from './components/dashboard-pagination/dashboard-pagination.component';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { PrimaryButtonComponent } from './components/primary-button/primary-button.component';
+import { SecondaryButtonComponent } from './components/secondary-button/secondary-button.component';
+import { FormFieldInputComponent } from './components/form-field-input/form-field-input.component';
 
 @NgModule({
   imports: [CommonModule, TranslateModule, FormsModule, NgxPaginationModule],
@@ -19,17 +22,22 @@ import { NgxPaginationModule } from 'ngx-pagination';
     DashboardSearchboxComponent,
     DialogFilterPipe,
     MenuComponent,
-    DashboardPaginationComponent
+    DashboardPaginationComponent,
+    PrimaryButtonComponent,
+    SecondaryButtonComponent,
+    FormFieldInputComponent
   ],
   exports: [
     DashboardItemsComponent,
     DashboardButtonsComponent,
     DashboardSearchboxComponent,
-    DashboardSearchboxComponent,
     ModalModule,
     DialogFilterPipe,
     DashboardPaginationComponent,
-    MenuComponent
+    MenuComponent,
+    PrimaryButtonComponent,
+    SecondaryButtonComponent,
+    FormFieldInputComponent
   ],
   providers: []
 })
