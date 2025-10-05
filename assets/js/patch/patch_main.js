a66311aa498e28777798ddccdbf58770663d4319
rbejar <>
2025-10-05 16:35:21 +0000

    Auto-generated commit [ci skip]
    Triggered by commit: https://github.com/sitmun/sitmun-viewer-app/commit/a94fb4eaf6be59d9c76348796c4a442bacbee0ac

--- assets/js/patch/patch_main.js
+++ assets/js/patch/patch_main.js
@@ -23,6 +23,7 @@ var arrayJsFiles = [
   //"assets/js/patch/SigSilmeOL.js",
   "assets/js/patch/SilmeMap.js",
   "assets/js/patch/SilmeModal.js",
+  "assets/js/patch/SilmeSecondBaseLayer.js",
   "assets/js/patch/SilmeUtils.js",
   "assets/js/patch/SilmeTree.js"
 ];
