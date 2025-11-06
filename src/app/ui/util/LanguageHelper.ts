3e4a694edd9c3b0fbd21d326a31df0fecdded6ba
damien chabret <dch@atolcd.com>
2025-11-06 11:15:45 +0100

    fix: language and css
    * add default languages to the selector if i18nService.availableLanguageCodes return nothing
    * change css of panl on map according to the navbar
    
    
    Change-Id: I1ef8568ee7449c178b16fa8fc02cb40d7790488e

--- src/app/ui/util/LanguageHelper.ts
+++ src/app/ui/util/LanguageHelper.ts
@@ -1,6 +1,6 @@
 import { I18nService } from '@api/services/i18n.service';
 import { Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +10,32 @@ export class LanguageHelper {
 
   constructor(private i18nService: I18nService) {}
 
+  /**
+   * Return a list of available languages.
+   * @returns A list of available languages. If the API call fails, it returns a default list.
+   */
   getLanguages(): Observable<LanguageDTO[]> {
-    return this.i18nService.availableLanguageCodes();
+    return this.i18nService.availableLanguageCodes().pipe(
+      map((languages: LanguageDTO[] | null | undefined) => {
+        if (!languages || languages.length === 0) {
+          return [
+            { name: 'es', shortName: 'ES', nativeName: 'Español' },
+            { name: 'en', shortName: 'EN', nativeName: 'English' },
+            { name: 'fr', shortName: 'FR', nativeName: 'Français' },
+            { name: 'ca', shortName: 'CA', nativeName: 'Català' }
+          ];
+        }
+        return languages;
+      }),
+      catchError(() =>
+        of([
+          { name: 'es', shortName: 'Español' },
+          { name: 'en', shortName: 'English' },
+          { name: 'fr', shortName: 'Français' },
+          { name: 'ca', shortName: 'Català' }
+        ])
+      )
+    );
   }
 }
 
