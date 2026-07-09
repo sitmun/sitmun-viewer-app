0b61b69a168a3dfa58f52e81c41f8315f83fd669
olamelas <olamelas@nexusgeographics.com>
2026-07-09 18:04:17 +0200

    Solve conflicts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   hashLocationStrategy: false,
   hideBackgroundImage: false,
   hideDNIEAccess: true,
-  apiUrl: 'http://localhost:4200/backend',
+  apiUrl: 'http://localhost:9000/backend',
   version: '1.2.8-SNAPSHOT',
   environmentName: 'Development',
   sitnaVersion: '4.8.0',
